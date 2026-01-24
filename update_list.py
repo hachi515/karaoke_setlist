868b43887dd78fc2fb33f161aa9a9447afadbacd
hachi515 <hachi515.tennis@gmail.com>
2026-01-24 16:53:49 +0900

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -45,6 +45,7 @@ room_map = {
     11086: "タカヒロ部屋",
     11087: "MiO部屋",
     11088: "ほっしー部屋",
+    11092: "ヒロ部屋",
     11101: "えみち部屋",
     11102: "るえ部屋",
     11103: "ながし部屋",
@@ -1073,3 +1074,4 @@ html_content = f"""
 with open("index.html", "w", encoding="utf-8") as f:
     f.write(html_content)
     print("HTML生成完了: index.html")
+
